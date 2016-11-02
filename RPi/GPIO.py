26e7253c69f783f7aafc1e2e7c38e0fe5f4e1ec8
bg <ben@graepel.info>
2016-11-02 21:36:50 +0100

    added RPi.GPIO directory for running app on local machine

---
+++ RPi/GPIO.py
@@ -0,0 +1,16 @@
+
+'''
+This file is used for developing on a loal machine
+which does not support the installation of the RPi.GPIO module
+'''
+
+def setmode(*arg):
+	pass
+def setup(*arg):
+	pass
+def output(*arg):
+	pass
+def BCM(*arg):
+	pass
+def OUT(*arg):
+	pass
---
+++ RPi/__init__.py
@@ -0,0 +1,12 @@
+
+'''
+This file is used for developing on a loal machine
+which does not support the installation of the RPi.GPIO module
+'''
+
+def setmode():
+	pass
+def setup():
+	pass
+def output():
+	pass
